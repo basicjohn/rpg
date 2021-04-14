344844daf1c3e3861f63e0856d12f22575a4a09c
basicjohn <edmondsonj@gmail.com>
2021-04-14 11:59:30 -0700

    failed the first test successfully
    Co-authored-by: Marney Mallory<marney.mallory@gmail.com>
    Co-authored-by: Cassandra Copp <cass16180@gmail.com>
    Co-authored-by: Brent Hubbard<hubbardbrent@hotmail.com>

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -1,25 +1,36 @@
-import Block from './../src/sudoku.js';
-import Sudoku from './../src/sudoku.js';
+// import Block from './../src/sudoku.js';
+import Player from './../src/player.js';
 
-describe('Block', () => {
+Describe('Player', () => {
 
-  test('should correctly create a Block object with block position keys', () => {
-    const block = new Block();
-    expect(block.pRow1).toHaveLength(0);
-    expect(block.pRow2).toHaveLength(0);
-    expect(block.pRow3).toHaveLength(0);
-  });
-});
+  test('should create a Player object',() => {
+    const player = new Player();
+  })
 
-describe('Block', () => {
+}
 
-  test('should correctly create a Block object with block position keys', () => {
-    const block = new Block();
-    expect(block.pRow1).toHaveLength(0);
-    expect(block.pRow2).toHaveLength(0);
-    expect(block.pRow3).toHaveLength(0);
-  });
-});
+)
+
+
+// describe('Block', () => {
+
+//   test('should correctly create a Block object with block position keys', () => {
+//     const block = new Block();
+//     expect(block.pRow1).toHaveLength(0);
+//     expect(block.pRow2).toHaveLength(0);
+//     expect(block.pRow3).toHaveLength(0);
+//   });
+// });
+
+// describe('Block', () => {
+
+//   test('should correctly create a Block object with block position keys', () => {
+//     const block = new Block();
+//     expect(block.pRow1).toHaveLength(0);
+//     expect(block.pRow2).toHaveLength(0);
+//     expect(block.pRow3).toHaveLength(0);
+//   });
+// });
 
 
 
