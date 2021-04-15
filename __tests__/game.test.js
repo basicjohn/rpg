66184d918fea5f11138d1b28d18bcd2411930beb
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-04-15 16:20:51 -0600

    adding more testing and beforeEach
    Co-authored-by: John Edmondson <edmondsonj@gmail.com>

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,49 +1,66 @@
+import Battle from '../src/js/battle.js';
 import Game from './../src/js/game.js';
 import { createGame } from './../src/js/game.js';
 
-describe('Game', () => {
-  const game = new Game();
+describe('Game', () => {  
+  beforeEach(() => {
+    const game = createGame(Game);
+    const battleContenders = game.battle();
+  });
 
   test('Create the game object using the Game constructor', () => {
-    expect(game)
-  })
+    expect(game);
+  });
 
   test('Create the game object filled with correct keys & values', () => {
     expect(game.players).toEqual([]);
     expect(game.monsters).toEqual([]);
     expect(game.distance).toEqual(0);
-  })
+  });
 
   test("Create prototype for the this.roll and get random number", () => {
     const roll = game.roll(12);
     expect(roll).not.toEqual(0);
-  })
+  });
 
   test("Create prototype for the this.move", () => {
-    expect(game.move)
-  })
+    expect(game.move);
+  });
 
   test("change distance by running game.move", () => {
-    game.move()
+    game.move();
     expect(game.distance).not.toEqual(0);
-  })
+  });
+
+  test('Test if battle is loading a player', () => {
+    const battlingPlayer = battleContenders[0];
+    expect(battlingPlayer).toEqual(game.players[1]);
+  });
+  test('test to see if battle is loading a battling monster', () => {
+    const battlingMonster = battleContenders[1];
+    const gameMonster = game.monsters[0];
+    expect(battlingMonster).toEqual(gameMonster);
+  });
 
 });
 
 
 
 describe('createGame', () => {
-  const game = createGame(Game);
+  beforeEach(() => {
+    const game = createGame(Game);
+    const battleContenders = game.battle();
+  });
 
   test('Create the game and test for distance', () => {
     expect(game.distance).toEqual(0);
-  })
+  });
 
   test('Create the game and add first play using createGame function', () => {
     expect(game.players[0].name).toMatch("Brentimous Maximus");
-  })
+  });
 
   test('See if the Giant Spider has been created', () => {
     expect(game.monsters[0].name).toMatch("Giant Spider");
-  })
+  });
 });
--- src/js/game.js
+++ src/js/game.js
@@ -9,31 +9,47 @@ export default class Game {
     this.distance = 0;
     this.maxDistance = 300;
   }
+
   // Getter
   move() {
     const moveDistance = this.roll(12);
     this.distance += moveDistance;
   }
+
+  addPlayer(player) {
+    this.players.push(player);
+  }
+
+  addMonster(monster) {
+    this.monsters.push(monster);
+  }
+
+  battle() {
+    let battlingMonster;
+    let battlingPlayer;
+    let numberOfMonsters = this.monsters.length;
+    let currentRoll = this.roll(numberOfMonsters);
+    battlingMonster = Object.assign({}, this.monsters[currentRoll]);
+    battlingPlayer = Object.assign({}, this.players[0]);
+    return [battlingPlayer, battlingMonster];
+
+  }
+
   // Helper
   roll(limit) {
     const currentRoll = Math.floor(Math.random() * limit) + 1;
     return currentRoll;
   }
 
-  addPlayer(player) {
-    this.players.push(player);
-  }
-  addMonster(monster) {
-    this.monsters.push(monster);
-  }
-
 };
 
 export function createGame(Game) {
   const player = new Player("Brentimous Maximus");
+  const player2 = new Player("Brutismous Maximum");
+  const player3 = new Player("Big Bad JOHN");
   const monster = new Monster("Giant Spider");
   const game = new Game();
-  
+
   game.addMonster(monster);
   game.addPlayer(player);
   return game;
