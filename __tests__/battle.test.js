491a983dd529b4a6027ecf188d686d8795418f4b
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-04-15 11:16:01 -0600

    good fail foe battle link to file
    Co-authored-by: John Edmondson <edmondsonj@gmail.com>

---
+++ __tests__/battle.test.js
@@ -0,0 +1,15 @@
+import Battle from './../src/js/battle.js';
+
+describe('Battle', () => {
+  const battle = new Battle();
+
+  test ('Create the battle object using the constructor', () => {
+    expect(battle)
+  })
+
+  test ('Create the Battle objest filled with correct keys and values', () => {
+expect(.....).toEqual([]);
+expect(.....).toEqual([]);
+expect(.....).toEqual([]);
+  })
+});
---
+++ src/js/battle.js
@@ -0,0 +1,5 @@
+export default class Battle {
+  constructor(){
+    this.
+  }
+}
